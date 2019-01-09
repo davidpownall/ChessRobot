c9b9cbfb9e40715e3cb3a83df7117780cfa4b2c6
David Pownall <davidtpownall@gmail.com>
2019-01-08 19:55:44 -0800

    Added some stubs for future work

--- inc/chessboard.h
+++ inc/chessboard.h
@@ -21,30 +21,37 @@
 #define BOARD_START_USED    0xffff00000000ffff
 #define BOARD_START_EMPTY   0x0000FFFFFFFF0000
 
+#define NUM_PIECE_TYPES     12
+
+typedef struct moveType_s
+{
+    void *nextMove;
+} moveType_t;
+
 /* Enum definining piece index */
 enum pieceType_t
 {
-    WHITE_PIECES,
-    BLACK_PIECES,
     WHITE_PAWN,
-    BLACK_PAWN,
     WHITE_ROOK,
-    BLACK_ROOK,
     WHITE_BISHOP,
-    BLACK_BISHOP,
     WHITE_KNIGHT,
-    BLACK_KNIGHT,
     WHITE_QUEEN,
-    BLACK_QUEEN,
     WHITE_KING,
-    BLACK_KING
+    BLACK_PAWN,
+    BLACK_ROOK,
+    BLACK_BISHOP,
+    BLACK_KNIGHT,
+    BLACK_QUEEN,
+    BLACK_KING,
+    WHITE_PIECES,
+    BLACK_PIECES
 };
 
 class ChessBoard
 {
 private:
     /* 12 unique bitboard representations required + two general */
-    uint64_t pieces[14];
+    uint64_t pieces[NUM_PIECE_TYPES + 2];
 
     /* Union of all bitboards */
     uint64_t occupied;
@@ -55,23 +62,35 @@ private:
 public:
 
     ChessBoard(void);
+    ChessBoard(uint64_t *pieces, uint64_t occupied);
 
     uint64_t getPiece(pieceType_t pt) const { return pieces[pt]; };
     uint64_t getWhitePieces() const { return pieces[WHITE_PIECES]; };
     uint64_t getBlackPieces() const { return pieces[BLACK_PIECES]; };
 
     uint64_t getWhitePawns() const { return pieces[WHITE_PAWN]; };
-    uint64_t getBlackPawns() const { return pieces[BLACK_PAWN]; };
     uint64_t getWhiteRooks() const { return pieces[WHITE_ROOK]; };
-    uint64_t getBlackRooks() const { return pieces[BLACK_ROOK]; };
     uint64_t getWhiteKnights() const { return pieces[WHITE_KNIGHT]; }; 
-    uint64_t getBlackKnights() const { return pieces[BLACK_KNIGHT]; };
     uint64_t getWhiteBishops() const { return pieces[WHITE_BISHOP]; }; 
-    uint64_t getBlackBishops() const { return pieces[BLACK_BISHOP]; };
     uint64_t getWhiteQueen() const { return pieces[WHITE_QUEEN]; };
-    uint64_t getBlackQueen() const { return pieces[BLACK_QUEEN]; };
     uint64_t getWhiteKing() const { return pieces[WHITE_KING]; };
+    
+    uint64_t getBlackPawns() const { return pieces[BLACK_PAWN]; };
+    uint64_t getBlackRooks() const { return pieces[BLACK_ROOK]; };
+    uint64_t getBlackKnights() const { return pieces[BLACK_KNIGHT]; };
+    uint64_t getBlackBishops() const { return pieces[BLACK_BISHOP]; };
+    uint64_t getBlackQueen() const { return pieces[BLACK_QUEEN]; };
     uint64_t getBlackKing() const { return pieces[BLACK_KING]; };
+
+    moveType_t generateMoves(pieceType_t pt);
+
+    void generatePawnMoves(pieceType_t pt, moveType_t *lastMove);
+    void generateRookMoves(pieceType_t pt, moveType_t *lastMove);
+    void generateBishopMoves(pieceType_t pt, moveType_t *lastMove);
+    void generateKnightMoves(pieceType_t pt, moveType_t *lastMove);
+    void generateQueenMoves(pieceType_t pt, moveType_t *lastMove);
+    void generateKingMoves(pieceType_t pt, moveType_t *lastMove);
+
 };
 
 uint64_t initializeChessBoard(void);
--- src/chessboard.cpp
+++ src/chessboard.cpp
@@ -1,24 +1,128 @@
+#include <iostream>
 #include "util.h"
 #include "chessboard.h"
 
 static ChessBoard *cb;
 
+/**
+ * Default constructor for the ChessBoard class. Creates a fresh board
+ * from scratch.
+ */
 ChessBoard::ChessBoard(void)
 {
+    uint32_t pt;
+
     this->pieces[WHITE_PAWN]    = WHITE_PAWN_START;
-    this->pieces[BLACK_PAWN]    = BLACK_PAWN_START;
     this->pieces[WHITE_ROOK]    = WHITE_ROOK_START;
-    this->pieces[BLACK_ROOK]    = BLACK_ROOK_START;
     this->pieces[WHITE_KNIGHT]  = WHITE_KNIGHT_START;
-    this->pieces[BLACK_KNIGHT]  = BLACK_KNIGHT_START;
     this->pieces[WHITE_BISHOP]  = WHITE_BISHOP_START;
-    this->pieces[BLACK_BISHOP]  = BLACK_BISHOP_START;
     this->pieces[WHITE_QUEEN]   = WHITE_QUEEN_START;
-    this->pieces[BLACK_QUEEN]   = BLACK_QUEEN_START;
     this->pieces[WHITE_KING]    = WHITE_KING_START;
+
+    this->pieces[BLACK_PAWN]    = BLACK_PAWN_START;
+    this->pieces[BLACK_ROOK]    = BLACK_ROOK_START;
+    this->pieces[BLACK_KNIGHT]  = BLACK_KNIGHT_START;
+    this->pieces[BLACK_BISHOP]  = BLACK_BISHOP_START;
+    this->pieces[BLACK_QUEEN]   = BLACK_QUEEN_START;
     this->pieces[BLACK_KING]    = BLACK_KING_START;
 
+    for(pt = 0; pt < NUM_PIECE_TYPES; ++pt)
+    {
+        if(pt < (NUM_PIECE_TYPES >> 1))
+        {
+            this->pieces[WHITE_PIECES] |= this->pieces[pt];
+        }
+        else
+        {
+            this->pieces[BLACK_PIECES] |= this->pieces[pt];
+        }
+        this->occupied |= this->pieces[pt];
+    }
+    this->empty = ~(this->occupied);
+}
 
+/**
+ * ChessBoard constructor which takes in an existing board state
+ * 
+ * @param *pieces:  The current board position of all pieces
+ * @param occupued: The set of all occupied squares
+ */
+ChessBoard::ChessBoard(uint64_t *pieces, uint64_t occupied)
+{
+
+    // Verify that our array is valid
+    if(pieces == NULL)
+    {
+        std::cout << "Error in piece array input during board construction"
+             << std::endl;
+        return;
+    } 
+
+    // Copy the board over
+    memcpy((void *) this->pieces, (void*) pieces, (NUM_PIECE_TYPES + 2) >> 1);
+
+    // @todo: Figure out a way to determine if an occupied bitboard is valid
+    this->occupied = occupied;
+    this->empty = ~occupied;
+
+}
+
+/**
+ * Generates the valid moves for a given chessboard state and color
+ * 
+ * @param pt:   The color you wish to generate possible moves for
+ */
+moveType_t ChessBoard::generateMoves(pieceType_t pt)
+{
+    moveType_t moveStart;
+    moveType_t lastMove;
+
+    // Determine validity of input
+    if(!(pt == WHITE_PIECES || pt == BLACK_PIECES))
+    {
+        std::cout << "Error in piece type input: " << pt << std::endl;
+        return moveStart;
+    }
+
+    // Generate possible plays
+    generatePawnMoves(pt, &lastMove);
+    generateRookMoves(pt, &lastMove);
+    generateBishopMoves(pt, &lastMove);
+    generateKnightMoves(pt, &lastMove);
+    generateQueenMoves(pt, &lastMove);
+    generateKingMoves(pt, &lastMove);
+
+    return moveStart;
+}
+
+void ChessBoard::generatePawnMoves(pieceType_t pt, moveType_t *lastMove)
+{
+    // @todo
+}
+
+void ChessBoard::generateRookMoves(pieceType_t pt, moveType_t *lastMove)
+{
+    // @todo
+}
+
+void ChessBoard::generateBishopMoves(pieceType_t pt, moveType_t *lastMove)
+{
+    // @todo
+}
+
+void ChessBoard::generateKnightMoves(pieceType_t pt, moveType_t *lastMove)
+{
+    // @todo
+}
+
+void ChessBoard::generateQueenMoves(pieceType_t pt, moveType_t *lastMove)
+{
+    // @todo
+}
+
+void ChessBoard::generateKingMoves(pieceType_t pt, moveType_t *lastMove)
+{
+    // @todo
 }
 
 uint64_t initializeChessBoard(void)
